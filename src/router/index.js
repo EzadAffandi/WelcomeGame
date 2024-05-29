8f067c37832d1131413acf4196eae3dd791e1cec
Ariff Shukor <ariff250178@gmail.com>
2024-05-29 16:16:39 +0200

    leaderboard

--- src/router/index.js
+++ src/router/index.js
@@ -32,6 +32,10 @@ const router = createRouter({
           path: 'search',
           component: () => import('../views/SearchPage.vue'),
         },
+        {
+          path: 'game',
+          component: () => import('../views/GamePage.vue'),
+        },
       ],
     },
   ]
